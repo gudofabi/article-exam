120f77cd261ecd722ab9a8fc5a69a0330361cd51
Gudo Fabi <godofredocpanel@gmail.com>
2024-11-04 16:52:38 +0800

    fix: dbjson

--- api/json-server.js
+++ api/json-server.js
@@ -2,18 +2,13 @@ import jsonServer from "json-server";
 import path from "path";
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../../db.json"));
+const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Updated path
 const middlewares = jsonServer.defaults();
-
-// Log request URL before routing
-server.use((req, res, next) => {
-  console.log(`Request URL: ${req.url}`);
-  next();
-});
 
 server.use(middlewares);
 server.use(router);
 
 export default (req, res) => {
+  console.log(`Request URL: ${req.url}`);
   server(req, res);
 };
