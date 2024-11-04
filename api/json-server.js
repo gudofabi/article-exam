8c7e76c0a97fa036b70a5edacd2e0904c6b5e606
Gudo Fabi <godofredocpanel@gmail.com>
2024-11-04 17:02:22 +0800

    fix: modify json-server

--- api/json-server.js
+++ api/json-server.js
@@ -1,14 +1,14 @@
-import jsonServer from "json-server";
-import path from "path";
+const jsonServer = require("json-server");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), "db.json")); // Updated path
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
-export default (req, res) => {
-  console.log(`Request URL: ${req.url}`);
-  server(req, res);
-};
+// Export the Server API
+module.exports = server;
